6b006abce94a578491a27eeca51f62bdb90b3052
somiceast <1474274420@qq.com>
2017-01-14 00:52:36 +0800

    终于encode出来了
    Signed-off-by: somiceast <1474274420@qq.com>

--- Python-crappy-novel.py
+++ Python-crappy-novel.py
@@ -1,32 +1,28 @@
-#http://www.imekaku.com/2015/08/31/python-work-tieba2/
-# -*- coding: utf-8 -*
+# coding=utf-8
 import re
 import urllib
 
 def getHtml(url):
+  page = urllib.urlopen(url)
+  html = page.read()
 
-    page = urllib.urlopen(url)
-    html = page.read()
-#    print html
-    return html
+  return html
+
 
 def getTxt(html):
-    regg='<div class="book_con">(\S*)<div class="pagination_wrap">'
-    # reg = r'src="(\S*\.jpg)"'
-    #正则表达式开始匹配
-    txtre = re.compile(regg)
-    #返回的是一个列表,所以将得到的所有楼层中的文本
-    # 得出,放在一个列表items中.最后依次打印出来就可以.
-    items = txtre.findall(html)
-    print items
+  regg = '<div class="book_con">([\s\S]*)<div class="pagination_wrap">'
+  # reg = r'src="(\S*\.jpg)"'
+  txtre = re.compile(regg)
+  # html = html.decode('utf-8').encode('gbk')
+  items = txtre.findall(html)
+  
+  print items[0].decode('utf-8')
+  removeImg = re.compile('<img.*?>')
+  removeImg = re.compile('<img.*?>')
+  removeImg = re.compile('<img.*?>')
 
 
-    # 去除标签
-    removeImg=re.compile('<img.*?>')
-    removeImg=re.compile('<img.*?>')
-    removeImg=re.compile('<img.*?>')
-
-url='http://lz.book.sohu.com/chapter-1363259.html'
-html=getHtml(url)
-getTxt(html)
+url = 'http://lz.book.sohu.com/chapter-1363259.html'
+htmld = getHtml(url)
+getTxt(htmld)
 print 'finished!'
--- Python-test.py
+++ Python-test.py
@@ -1,20 +1,22 @@
 import re
 import urllib
 
+
 def getHtml(url):
     page = urllib.urlopen(url)
     html = page.read()
     return html
 
+
 def getImg(html):
     reg = r'src="(\S*\.jpg)"'
     imgre = re.compile(reg)
-    imglistjpg = re.findall(imgre,html)
+    imglistjpg = re.findall(imgre, html)
     # return imglist
     print imglistjpg
     x = 0
     for imgurl in imglistjpg:
-        urllib.urlretrieve(imgurl,'%s.jpg' % x)
+       # urllib.urlretrieve(imgurl, '%s.jpg' % x)
         x+=1
 
 url = "http://image.baidu.com/"
